62c21523e6ddcd4905504e523b3a316558757a99
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-15 02:02:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,14 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+  post '/newteam' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+  end
+
 end
